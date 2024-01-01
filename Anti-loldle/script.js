f2a25adf120e598ab9bf6bfb8b6f3b5ab4cd66b2
Antoine Haddad <89608252+YourAntidote@users.noreply.github.com>
2024-01-01 17:25:42 -0500

    The Beginning!

---
+++ Anti-loldle/script.js
@@ -0,0 +1,211 @@
+var pictureUrls = [
+    "./champions/aatrox.jpg",
+    "./champions/ahri.jpg",
+    "./champions/akali.jpg",
+    "./champions/RiotX_ChampionList_akshan.jpg",
+    "./champions/RiotX_ChampionList_alistar.jpg",
+    "./champions/RiotX_ChampionList_amumu.jpg",
+    "./champions/RiotX_ChampionList_anivia.jpg",
+    "./champions/RiotX_ChampionList_annie.jpg",
+    "./champions/RiotX_ChampionList_aphelios.jpg",
+    "./champions/RiotX_ChampionList_ashe.jpg",
+    "./champions/RiotX_ChampionList_aurelionsol.jpg",
+    "./champions/RiotX_ChampionList_azir.jpg",
+    "./champions/RiotX_ChampionList_bard.jpg",
+    "./champions/RiotX_ChampionList_belveth.jpg",
+    "./champions/RiotX_ChampionList_blitzcrank.jpg",
+    "./champions/RiotX_ChampionList_brand.jpg",
+    "./champions/RiotX_ChampionList_braum.jpg",
+    "./champions/RiotX_ChampionList_briar.jpg",
+    "./champions/RiotX_ChampionList_caitlyn.jpg",
+    "./champions/RiotX_ChampionList_camille.jpg",
+    "./champions/RiotX_ChampionList_cassiopeia.jpg",
+    "./champions/RiotX_ChampionList_chogath.jpg",
+    "./champions/RiotX_ChampionList_corki.jpg",
+    "./champions/RiotX_ChampionList_darius.jpg",
+    "./champions/RiotX_ChampionList_diana.jpg",
+    "./champions/RiotX_ChampionList_dramundo.jpg",
+    "./champions/RiotX_ChampionList_draven.jpg",
+    "./champions/RiotX_ChampionList_ekko.jpg",
+    "./champions/RiotX_ChampionList_elise.jpg",
+    "./champions/RiotX_ChampionList_evelynn.jpg",
+    "./champions/RiotX_ChampionList_ezreal.jpg",
+    "./champions/RiotX_ChampionList_fiddlesticks.jpg",
+    "./champions/RiotX_ChampionList_fiora.jpg",
+    "./champions/RiotX_ChampionList_fizz.jpg",
+    "./champions/RiotX_ChampionList_galio.jpg",
+    "./champions/RiotX_ChampionList_gangplank.jpg",
+    "./champions/RiotX_ChampionList_garen.jpg",
+    "./champions/RiotX_ChampionList_gnar.jpg",
+    "./champions/RiotX_ChampionList_gragas.jpg",
+    "./champions/RiotX_ChampionList_graves.jpg",
+    "./champions/RiotX_ChampionList_gwen.jpg",
+    "./champions/RiotX_ChampionList_hecarim.jpg",
+    "./champions/RiotX_ChampionList_heimerdinger.jpg",
+    "./champions/RiotX_ChampionList_hwei.jpg",
+    "./champions/RiotX_ChampionList_illaoi.jpg",
+    "./champions/RiotX_ChampionList_irelia.jpg",
+    "./champions/RiotX_ChampionList_ivern.jpg",
+    "./champions/RiotX_ChampionList_janna.jpg",
+    "./champions/RiotX_ChampionList_jarvaniv.jpg",
+    "./champions/RiotX_ChampionList_jax.jpg",
+    "./champions/RiotX_ChampionList_jayce.jpg",
+    "./champions/RiotX_ChampionList_jhin.jpg",
+    "./champions/RiotX_ChampionList_jinx.jpg",
+    "./champions/RiotX_ChampionList_ksante.jpg",
+    "./champions/RiotX_ChampionList_kaisa.jpg",
+    "./champions/RiotX_ChampionList_kalista.jpg",
+    "./champions/RiotX_ChampionList_karma.jpg",
+    "./champions/RiotX_ChampionList_karthus.jpg",
+    "./champions/RiotX_ChampionList_kassadin.jpg",
+    "./champions/RiotX_ChampionList_katarina.jpg",
+    "./champions/RiotX_ChampionList_kayle.jpg",
+    "./champions/RiotX_ChampionList_kayn.jpg",
+    "./champions/RiotX_ChampionList_kennen.jpg",
+    "./champions/RiotX_ChampionList_khazix.jpg",
+    "./champions/RiotX_ChampionList_kindred.jpg",
+    "./champions/RiotX_ChampionList_kled.jpg",
+    "./champions/RiotX_ChampionList_kogmaw.jpg",
+    "./champions/RiotX_ChampionList_leblanc.jpg",
+    "./champions/RiotX_ChampionList_leesin.jpg",
+    "./champions/RiotX_ChampionList_leona.jpg",
+    "./champions/RiotX_ChampionList_lillia.jpg",
+    "./champions/RiotX_ChampionList_lissandra.jpg",
+    "./champions/RiotX_ChampionList_lucian.jpg",
+    "./champions/RiotX_ChampionList_lulu.jpg",
+    "./champions/RiotX_ChampionList_lux.jpg",
+    "./champions/RiotX_ChampionList_malphite.jpg",
+    "./champions/RiotX_ChampionList_malzahar.jpg",
+    "./champions/RiotX_ChampionList_maokai.jpg",
+    "./champions/RiotX_ChampionList_masteryi.jpg",
+    "./champions/RiotX_ChampionList_milio.jpg",
+    "./champions/RiotX_ChampionList_missfortune.jpg",
+    "./champions/RiotX_ChampionList_mordekaiser.jpg",
+    "./champions/RiotX_ChampionList_morgana.jpg",
+    "./champions/RiotX_ChampionList_naafiri.jpg",
+    "./champions/RiotX_ChampionList_nami.jpg",
+    "./champions/RiotX_ChampionList_nasus.jpg",
+    "./champions/RiotX_ChampionList_nautilus.jpg",
+    "./champions/RiotX_ChampionList_neeko.jpg",
+    "./champions/RiotX_ChampionList_nidalee.jpg",
+    "./champions/RiotX_ChampionList_nilah.jpg",
+    "./champions/RiotX_ChampionList_nocturne.jpg",
+    "./champions/RiotX_ChampionList_nunu.jpg",
+    "./champions/RiotX_ChampionList_olaf.jpg",
+    "./champions/RiotX_ChampionList_orianna.jpg",
+    "./champions/RiotX_ChampionList_ornn.jpg",
+    "./champions/RiotX_ChampionList_pantheon.jpg",
+    "./champions/RiotX_ChampionList_poppy.jpg",
+    "./champions/RiotX_ChampionList_pyke.jpg",
+    "./champions/RiotX_ChampionList_qiyana.jpg",
+    "./champions/RiotX_ChampionList_quinn.jpg",
+    "./champions/RiotX_ChampionList_rakan.jpg",
+    "./champions/RiotX_ChampionList_rammus.jpg",
+    "./champions/RiotX_ChampionList_reksai.jpg",
+    "./champions/RiotX_ChampionList_rell.jpg",
+    "./champions/RiotX_ChampionList_renata.jpg",
+    "./champions/RiotX_ChampionList_renekton.jpg",
+    "./champions/RiotX_ChampionList_rengar.jpg",
+    "./champions/RiotX_ChampionList_riven.jpg",
+    "./champions/RiotX_ChampionList_rumble.jpg",
+    "./champions/RiotX_ChampionList_ryze.jpg",
+    "./champions/RiotX_ChampionList_samira.jpg",
+    "./champions/RiotX_ChampionList_sejuani.jpg",
+    "./champions/RiotX_ChampionList_senna.jpg",
+    "./champions/RiotX_ChampionList_seraphine.jpg",
+    "./champions/RiotX_ChampionList_sett.jpg",
+    "./champions/RiotX_ChampionList_shaco.jpg",
+    "./champions/RiotX_ChampionList_shen.jpg",
+    "./champions/RiotX_ChampionList_shyvana.jpg",
+    "./champions/RiotX_ChampionList_singed.jpg",
+    "./champions/RiotX_ChampionList_sion.jpg",
+    "./champions/RiotX_ChampionList_sivir.jpg",
+    "./champions/RiotX_ChampionList_skarner.jpg",
+    "./champions/RiotX_ChampionList_sona.jpg",
+    "./champions/RiotX_ChampionList_soraka.jpg",
+    "./champions/RiotX_ChampionList_swain.jpg",
+    "./champions/RiotX_ChampionList_sylas.jpg",
+    "./champions/RiotX_ChampionList_syndra.jpg",
+    "./champions/RiotX_ChampionList_tahmkench.jpg",
+    "./champions/RiotX_ChampionList_taliyah.jpg",
+    "./champions/RiotX_ChampionList_talon.jpg",
+    "./champions/taric.jpg",
+    "./champions/teemo.jpg",
+    "./champions/thresh.jpg",
+    "./champions/tristana.jpg",
+    "./champions/trundle.jpg",
+    "./champions/tryndamere.jpg",
+    "./champions/twf.jpg",
+    "./champions/twitch.jpg",
+    "./champions/udyr.jpg",
+    "./champions/urgot.jpg",
+    "./champions/varus.jpg",
+    "./champions/vayne.jpg",
+    "./champions/veigar.jpg",
+    "./champions/velkoz.jpg",
+    "./champions/vex.jpg",
+    "./champions/vi.jpg",
+    "./champions/viego.jpg",
+    "./champions/viktor.jpg",
+    "./champions/vladimir.jpg",
+    "./champions/Volibear.jpg",
+    "./champions/warwick.jpg",
+    "./champions/wukong.jpg",
+    "./champions/xayah.jpg",
+    "./champions/xerath.jpg",
+    "./champions/xinzhao.jpg",
+    "./champions/yasuo.jpg",
+    "./champions/Yone.jpg",
+    "./champions/yorick.jpg",
+    "./champions/yuumi.jpg",
+    "./champions/zac.jpg",
+    "./champions/zed.jpg",
+    "./champions/zeri.jpg",
+    "./champions/ziggs.jpg",
+    "./champions/zilean.jpg",
+    "./champions/zoe.jpg",
+    "./champions/zyra.jpg",
+];
+
+
+
+
+var carteContainer = document.getElementById("carteContainer");
+let btn = document.querySelector("#btn");
+let undo = document.querySelector("#undo");
+
+for (var i = 0; i < 166; i++) {
+    var carte = document.createElement("div");
+    carte.className = "carte champion";
+    var img = document.createElement("img");
+    img.src = pictureUrls[i % pictureUrls.length];
+    carte.appendChild(img);
+    carteContainer.appendChild(carte);
+    console.log("Il y a " + (i + 1) + " champions.")
+}
+
+const allCardsArr = document.querySelectorAll(".carte");
+
+btn.addEventListener('click', function () {
+    reset();
+})
+
+function reset() {
+    allCardsArr.forEach(function(carte, i) {
+        allCardsArr[i].classList.remove("invisible");
+    }
+)};
+
+let lastDelete;
+
+allCardsArr.forEach(function(carte, i) {
+
+    carte.addEventListener('click', function () {
+        allCardsArr[i].classList.add('invisible');
+        lastDelete = allCardsArr[i];
+    });
+});
+
+undo.addEventListener('click', function(){
+    lastDelete.classList.remove('invisible');
+})
